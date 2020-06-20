e828b4559cc04ecf769f5e427009fa5c7001a76a
Alex Rosenfeld <arosenfeld2003@mac.com>
2020-06-19 17:56:56 -0700

    Update my_printf.c

--- my_printf.c
+++ my_printf.c
@@ -3,6 +3,12 @@
 #include <stdarg.h>
 #include <string.h>
 
+/*
+  My implementation of the C library (stdio.h) method 'printf'.
+  Includes conversions for %d, %o, %d, %x, %c, %s, and %p.
+  Includes my implementation of a method to convert a number of any base to another base (e.g. decimal to hexadecimal).
+*/
+
 // print %d or %i.
 int printnumber(int n) {
   int len = 0;
